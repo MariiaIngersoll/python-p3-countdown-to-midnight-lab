6725fce17e461ca52526c920a0d20ce6359ddc35
+ <+>
2023-05-29 21:56:13 -0400

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,24 @@
-# your code goes here!
+import time
+
+def countdown(number):
+    i = number
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+    if i == 0:
+        print("HAPPY NEW YEAR")
+        
+countdown(10)
+
+
+
+def countdown(number):
+    i = number
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        time.sleep(1)
+        i -= 1
+    if i == 0:
+        print("HAPPY NEW YEAR")
+        
+countdown(10)
